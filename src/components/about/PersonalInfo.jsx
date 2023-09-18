c1ff5241770535cfa5020ee16f85b087d010ba9d
Enrique Coronado <encodari@gmail.com>
2023-09-18 15:58:01 -0600

    Init

---
+++ src/components/about/PersonalInfo.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const personalInfoContent = [
+  { meta: "first name", metaInfo: "Enrique" },
+  { meta: "last name", metaInfo: "Coronado" },
+  { meta: "Age", metaInfo: "41 Years" },
+  { meta: "Nationality", metaInfo: "Mexican" },
+  { meta: "Available", metaInfo: "Yes" },
+  { meta: "Address", metaInfo: "Mexico" },
+  { meta: "phone", metaInfo: "+529996491746" },
+  { meta: "Email", metaInfo: "encodari@gmail.com" },
+  // { meta: "Skype", metaInfo: " enrique" },
+  { meta: "langages", metaInfo: "Spanish, English" },
+];
+
+const PersonalInfo = () => {
+  return (
+    <ul className="about-list list-unstyled open-sans-font">
+      {personalInfoContent.map((val, i) => (
+        <li key={i}>
+          <span className="title">{val.meta}: </span>
+          <span className="value d-block d-sm-inline-block d-lg-block d-xl-inline-block">
+            {val.metaInfo}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PersonalInfo;
