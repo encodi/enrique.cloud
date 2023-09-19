03af8bcb676548bb1b08e969eed712de5418d086
Enrique Coronado <encodari@gmail.com>
2023-09-19 09:27:51 -0600

    Init - Test of ci/cd (#1)
    - Initial ci/cd test

---
+++ src/components/Address.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Address = () => {
+  return (
+    <>
+      <p className="open-sans-font custom-span-contact position-relative">
+        <i className="fa fa-map position-absolute"></i>
+        <span className="d-block">Address Point</span>Gonzalo Guerrero, Merida, Yucatan, Mexico.
+      </p>
+      {/* End .custom-span-contact */}
+
+      <p className="open-sans-font custom-span-contact position-relative">
+        <i className="fa fa-envelope-open position-absolute"></i>
+        <span className="d-block">mail me</span>{" "}
+        <a href="mailto:encodari@gmail.com">encodari@gmail.com</a>
+      </p>
+      {/* End .custom-span-contact */}
+
+      <p className="open-sans-font custom-span-contact position-relative">
+        <i className="fa fa-phone-square position-absolute"></i>
+        <span className="d-block">call me</span>{" "}
+        <a href="Tel: +216 21 184 010">+529996491746</a>
+      </p>
+      {/* End .custom-span-contact */}
+    </>
+  );
+};
+
+export default Address;
